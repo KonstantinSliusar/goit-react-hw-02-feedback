1c0eace201f7fc0c075eed227e5f53bcb1c20d10
Konstantin <Konstantin.sl1982@gmail.com>
2021-04-29 09:32:35 +0300

    Updates

---
+++ precache-manifest.32be574b90fd212ca63f1afd3629ba14.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bae20466a2fe6414edc6aa958c452a1d",
+    "url": "/goit-react-hw-02-feedback/index.html"
+  },
+  {
+    "revision": "9acf4f9202669e5ac4c2",
+    "url": "/goit-react-hw-02-feedback/static/css/main.abf57fa9.chunk.css"
+  },
+  {
+    "revision": "60515829eabbf3a966f0",
+    "url": "/goit-react-hw-02-feedback/static/js/2.bf0da08d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-feedback/static/js/2.bf0da08d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9acf4f9202669e5ac4c2",
+    "url": "/goit-react-hw-02-feedback/static/js/main.5fef63fe.chunk.js"
+  },
+  {
+    "revision": "cb08e2ec47267fd4ee9f",
+    "url": "/goit-react-hw-02-feedback/static/js/runtime-main.9cb7e66e.js"
+  }
+]);
